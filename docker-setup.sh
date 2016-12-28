a2355b41b8ab067b5f3080ca88f8a4b1db4f1d68
Mark B <marklon@gmail.com>
2016-12-27 22:43:00 -0800

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 # Docker setup with script
-# Source http://tecadmin.net/install-oracle-virtualbox-on-centos-redhat-and-fedora/
-# First install virtual box
 # Install with the script
 $ sudo yum update
 $ curl -fsSL https://get.docker.com/ | sh
