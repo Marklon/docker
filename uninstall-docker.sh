177fd3d428004393bbe00462cec118e9520a5456
Mark B <marklon@gmail.com>
2016-12-29 14:53:25 -0800

    Update uninstall-docker.sh
    Removed space between #! and /bin/bash

--- uninstall-docker.sh
+++ uninstall-docker.sh
@@ -1,10 +1,10 @@
-#! /bin/bash
+#!/bin/bash
 # Uninstall You can uninstall the Docker software with yum.
-$ yum list installed | grep docker
+yum list installed | grep docker
 # Remove the package
-$ sudo yum -y remove docker-engine.x86_64
-$ sudo yum -y remove docker-engine-selinux.noarch
+sudo yum -y remove docker-engine.x86_64
+sudo yum -y remove docker-engine-selinux.noarch
 # To delete all images, containers, and volumes, run the following command:
-$ rm -rf /var/lib/docker
+rm -rf /var/lib/docker
 
 
