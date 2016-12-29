fe70cfd58b4546d43dde4053752461c266505df9
Mark B <marklon@gmail.com>
2016-12-29 14:48:47 -0800

    Update docker-setup.sh
    Removed $ so the script can run properly

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
 # This script will install Docker along with Docker-Compose
 # Install with the script
-$ sudo yum update
-$ curl -fsSL https://get.docker.com/ | sh
-$ sudo systemctl enable docker
-$ sudo systemctl start docker
+ sudo yum update
+ curl -fsSL https://get.docker.com/ | sh
+ sudo systemctl enable docker
+ sudo systemctl start docker
 # Verify docker is installed correctly by running a test image in a container.
-$ sudo systemctl start docker
+ sudo systemctl start docker
 # Docker-Compose setup with script
 
- $ curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+ curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o \
+ /usr/local/bin/docker-compose
 
 # Apply executable permissions to the binary:
-$ chmod +x /usr/local/bin/docker-compose
+ chmod +x /usr/local/bin/docker-compose
 # Test the installation.
- $ docker-compose --version
+ docker-compose --version
  docker-compose version: 1.9.0
