761dadff03651c9833800f90142bc414ffe62a29
Mark B <marklon@gmail.com>
2017-01-20 00:41:35 -0800

    removed line 19
    removed line showing docker-compose --version

--- docker-setup.sh
+++ docker-setup.sh
@@ -9,6 +9,8 @@
  sudo systemctl start docker
 # Docker-Compose setup with script
 
+wait
+
  curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o \
  /usr/local/bin/docker-compose
 
@@ -16,4 +18,3 @@
  chmod +x /usr/local/bin/docker-compose
 # Test the installation.
  docker-compose --version
- docker-compose version: 1.9.0
